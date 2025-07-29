0701f1649053282d43c4e75cc48af9f74a454101
dhiman10 <dhimandas10.sec@gmail.com>
2025-07-29 12:41:54 +0200

    Update SwiftUI project

---
+++ FirebaseBootcamp/FirebaseBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseBootcamp
+//
+//  Created by Dhiman Das on 29.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseBootcamp/FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Dhiman Das on 29.07.25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
